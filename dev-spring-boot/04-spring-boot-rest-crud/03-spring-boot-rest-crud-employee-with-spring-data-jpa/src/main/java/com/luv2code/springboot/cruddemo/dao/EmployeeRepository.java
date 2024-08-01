a13390f59b1b86681b44584930bc60fec2d79736
Abdallah Obiedat <162734556+AbdallahOb@users.noreply.github.com>
2024-08-01 17:16:07 +0300

    Delete dev-spring-boot directory

--- dev-spring-boot/04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springboot.cruddemo.dao;
-
-import com.luv2code.springboot.cruddemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    // that's it ... no need to write any code LOL!
-    // Using this you have get all the crud operations without DAO and DAOImpl
-}
